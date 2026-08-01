08fbe4eaa5f683bebba23ec502e12d0de7a7a615
rinchao0721 <linjiang86868686@gmail.com>
2026-08-01 22:51:12 +0800

    build: 新增 androidx.appcompat 依赖，为官方每应用语言支持做准备

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -156,6 +156,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
     // Navigation
