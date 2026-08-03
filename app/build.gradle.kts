896f4b64c2b98960565a5ada2b8db36a4a50e6e5
rinchao0721 <linjiang86868686@gmail.com>
2026-08-03 22:20:19 +0800

    chore: 版本号提升至 0.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.nhviewer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.0"
+        versionCode = 7
+        versionName = "0.3.5"
 
         testInstrumentationRunner = "com.example.nhviewer.HiltTestRunner"
     }
