d384647618ea73c38633e7d472bdbdfa304bf9c8
segfault-yu <mizumiya0721@gmail.com>
2026-08-24 22:20:14 +0800

    chore: 版本号提升至 0.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.nhviewer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.3.5"
+        versionCode = 8
+        versionName = "0.3.6"
 
         testInstrumentationRunner = "com.example.nhviewer.HiltTestRunner"
     }
